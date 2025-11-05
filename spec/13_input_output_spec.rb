07b3804d562192528619cd24f3f4bd02ba38dda4
Maithree de Silva <90709583+maithd78@users.noreply.github.com>
2025-11-05 20:14:14 +0900

    completed assignment

--- spec/13_input_output_spec.rb
+++ spec/13_input_output_spec.rb
@@ -178,9 +178,15 @@ describe NumberGame do
     # Write a test for the following context.
     context 'when user inputs two incorrect values, then a valid input' do
       before do
+        letter1 = 'a'
+        letter2 = 'b'
+        valid_input = '6'
+        allow(game_loop).to receive(:player_input).and_return(letter1, letter2, valid_input)
       end
 
-      xit 'completes loop and displays error message twice' do
+      it 'completes loop and displays error message twice' do
+        expect(game_loop).to receive(:puts).with('Input error!').twice
+        game_loop.player_turn
       end
     end
   end
@@ -211,8 +217,9 @@ describe NumberGame do
     # Create a new instance of NumberGame, with specific values for @solution,
     # @guess, and @count
     context 'when count is 2-3' do
+      subject(:game) { described_class.new(3, '4', 3) }
       # remove the 'x' before running this test
-      xit 'outputs correct phrase' do
+      it 'outputs correct phrase' do
         congrats_phrase = "Congratulations! You picked the random number in 3 guesses!\n"
         expect { game.final_message }.to output(congrats_phrase).to_stdout
       end
@@ -222,8 +229,11 @@ describe NumberGame do
 
     # Write a test for the following context.
     context 'when count is 4 and over' do
+      subject(:game_three) { described_class.new(3, '3', 5) }
       # remove the 'x' before running this test
-      xit 'outputs correct phrase' do
+      it 'outputs correct phrase' do
+        game_over_phrase = "That was hard. It took you 5 guesses!\n"
+        expect { game_three.final_message }.to output(game_over_phrase).to_stdout
       end
     end
   end
